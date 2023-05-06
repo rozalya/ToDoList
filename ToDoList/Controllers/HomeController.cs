171b4232e6fcac37fc5fe63b9cd38e204a8c9ea3
Rozaliya Evtimova <rosalia_evtimova@abv.bg>
2023-05-06 12:53:57 +0300

    Login form

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using ToDoList.Models;
 
 namespace ToDoList.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         private readonly ILogger<HomeController> _logger;
 
